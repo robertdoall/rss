ec0c40d09caa980a4bb0a8d25a6ec9db3eda6806
Admin <snorre@Snorres-MacBook-Pro-2.local>
2023-05-13 17:32:52 -0600

    update podcast list

--- index2.js
+++ index2.js
@@ -14,23 +14,23 @@ let parser = new Parser({
 });
 
 const feedUrls = [
-  // "https://achtungamerikaner.libsyn.com/rss",
-  // "https://audio.nobodyhasthe.biz/api/v1/channels/ascendingthespectrum_official/rss",
-  // "https://audio.nobodyhasthe.biz/api/v1/channels/thefinalstormishere/rss",
-  // "https://anchor.fm/s/5b4028c8/podcast/rss", // great men of our history  --nope
-  // "https://media.rss.com/johnfashcroft/feed.xml",
-  // "https://anchor.fm/s/7bbe76b8/podcast/rss", // this time the multiverse  --nope
-  // "https://feeds.libsyn.com/325607/rss", // hate house
-  // "https://anchor.fm/s/9510ca1c/podcast/rss", // audio invictus            --nope
-  // "https://anchor.fm/s/b68e7bbc/podcast/rss", // the white corner          --nope
-  // "https://fullhaus.libsyn.com/rss",
-  // "https://feeds.libsyn.com/106598/rss", // godcast
-  // "https://anchor.fm/s/b4c7199c/podcast/rss", // involution
-  // "https://audio.nobodyhasthe.biz/api/v1/channels/therealsmokepit/rss",
-  // "https://audio.nobodyhasthe.biz/api/v1/channels/thegamerword/rss",
-  // "https://odysee.com/$/rss/@FizeekFriday:d", // odysee files are too big.... YIKES
-  // "https://feeds.feedburner.com/archive/littlewarspodcast",
-  // "https://anchor.fm/s/3f92428c/podcast/rss", // daily decade
+  "https://achtungamerikaner.libsyn.com/rss",
+  "https://audio.nobodyhasthe.biz/api/v1/channels/ascendingthespectrum_official/rss",
+  "https://audio.nobodyhasthe.biz/api/v1/channels/thefinalstormishere/rss",
+  "https://anchor.fm/s/5b4028c8/podcast/rss", // great men of our history  --nope
+  "https://media.rss.com/johnfashcroft/feed.xml",
+  "https://anchor.fm/s/7bbe76b8/podcast/rss", // this time the multiverse  --nope
+  "https://feeds.libsyn.com/325607/rss", // hate house
+  "https://anchor.fm/s/9510ca1c/podcast/rss", // audio invictus            --nope
+  "https://anchor.fm/s/b68e7bbc/podcast/rss", // the white corner          --nope
+  "https://fullhaus.libsyn.com/rss",
+  "https://feeds.libsyn.com/106598/rss", // godcast
+  "https://anchor.fm/s/b4c7199c/podcast/rss", // involution
+  "https://audio.nobodyhasthe.biz/api/v1/channels/therealsmokepit/rss",
+  "https://audio.nobodyhasthe.biz/api/v1/channels/thegamerword/rss",
+  "https://odysee.com/$/rss/@FizeekFriday:d", // odysee files are too big.... YIKES
+  "https://feeds.feedburner.com/archive/littlewarspodcast",
+  "https://anchor.fm/s/3f92428c/podcast/rss", // daily decade
   "https://audio.nobodyhasthe.biz/api/v1/channels/amerikanercommunityradio/rss",
   "https://audio.nobodyhasthe.biz/api/v1/channels/exodus_americanus/rss",
 
